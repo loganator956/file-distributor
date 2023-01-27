7b1b9edca5d96b7635f574cdf30e59a7b5203091
Logan King <65763483+loganator956@users.noreply.github.com>
2023-01-27 21:56:14 +0000

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,11 +1,7 @@
-cd src
-
 dotnet publish -c Release --self-contained true -r win-x64
 dotnet publish -c Release --self-contained true -r linux-x64
-dotnet publish -c Release --self-contained true -r linux-arm
 dotnet publish -c Release --self-contained true -r linux-arm64
 
-Compress-Archive -Path ".\bin\Release\net6.0\win-x64\publish\file-distributor.exe" -CompressionLevel "Fastest" -Force -DestinationPath "./bin/file-distributor.win-x64.zip"
-Compress-Archive -Path ".\bin\Release\net6.0\linux-x64\publish\file-distributor" -CompressionLevel "Fastest" -Force -DestinationPath "./bin/file-distributor.linux-x64.zip"
-Compress-Archive -Path ".\bin\Release\net6.0\linux-arm\publish\file-distributor" -CompressionLevel "Fastest" -Force -DestinationPath "./bin/file-distributor.linux-arm.zip"
-Compress-Archive -Path ".\bin\Release\net6.0\linux-arm64\publish\file-distributor" -CompressionLevel "Fastest" -Force -DestinationPath "./bin/file-distributor.linux-arm64.zip"
+Compress-Archive -Path ".\src\\bin\Release\net6.0\win-x64\publish\file-distributor.exe" -CompressionLevel "Fastest" -Force -DestinationPath "./src/bin/file-distributor.win-x64.zip"
+Compress-Archive -Path ".\src\\bin\Release\net6.0\linux-x64\publish\file-distributor" -CompressionLevel "Fastest" -Force -DestinationPath "./src/bin/file-distributor.linux-x64.zip"
+Compress-Archive -Path ".\src\bin\Release\net6.0\linux-arm64\publish\file-distributor" -CompressionLevel "Fastest" -Force -DestinationPath "./src/bin/file-distributor.linux-arm64.zip"
