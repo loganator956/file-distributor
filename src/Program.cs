1b341bbb8be8148be4afa41f7ea00c077526087e
Logan King <65763483+loganator956@users.noreply.github.com>
2023-02-03 23:36:09 +0000

    Add verbosity levels

--- src/Program.cs
+++ src/Program.cs
@@ -8,8 +8,7 @@ Version appVersion = Assembly.GetExecutingAssembly().GetName().Version ?? new Ve
 string versionString = $"V{appVersion.Major}.{appVersion.Minor}.{appVersion.Build}.{appVersion.Revision}";
 Console.WriteLine($"file-distributor version {versionString}\n");
 
-const int DefaultVerbosity = 1;
-int verbosityLevel = 1;
+int verbosityLevel = 0;
 
 // Data size conversions
 const long GigabyteSize = 1024L * 1024L * 1024L;
@@ -80,6 +79,18 @@ if (_tempArg is not null)
 if (monitorWaitSeconds <= 0)
 {
     monitorWaitSeconds = MonitorWaitSecondsDefault;
+}
+
+List<Argument> verboseArgs = arguments.FindAll(x => x.Name == "verbose");
+if (verboseArgs.Count > 1)
+    verbosityLevel = verboseArgs.Count;
+else if (verboseArgs.Count == 1)
+{ 
+    if (!int.TryParse(verboseArgs[0].Value, out verbosityLevel))
+    {
+        // couldn't parse the argument value, so just assuming called it as "-v"
+        verbosityLevel = 1;
+    }
 }
 ;
 // ignore information
@@ -152,10 +163,10 @@ while (enableMonitorMode)
 Print(@$"Config:
 Folder A: {aPath}
 Folder B: {bPath}
-Size of A: {sizeGB}", 1);
+Size of A: {sizeGB}", 0);
 foreach(string keyword in ignoredKeywords)
 {
-    Print($"Ignored Keyword: {keyword}", 2);
+    Print($"Ignored Keyword: {keyword}", 1);
 }
 
 void DistributeFiles()
@@ -233,13 +244,13 @@ void TryMoveFile(FileInfo file, string destinationPath, bool isATarget)
     string parentDirectoryPath = Path.GetDirectoryName(destinationPath) ?? string.Empty;
     if (!Directory.Exists(parentDirectoryPath) && !string.IsNullOrEmpty(parentDirectoryPath))
         Directory.CreateDirectory(parentDirectoryPath);
-    Print($"[{(isATarget ? "B -> A" : "A -> B")}] {file.FullName} TO {destinationPath}", 1);
+    Print($"[{(isATarget ? "B -> A" : "A -> B")}] {file.FullName} TO {destinationPath}", 0);
     file.MoveTo(destinationPath, false);
 }
 
 List<string> GetFiles(string path)
 {
-    Print($"Discovering Files: {path}", 2);
+    Print($"Discovering Files: {path}", 1);
     List<string> files = new List<string>();
     files.AddRange(Directory.GetFiles(path));
     foreach (string subDir in Directory.GetDirectories(path))
